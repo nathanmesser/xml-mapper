8fe947cb8eccff95e6ea7bc945a39a6cec2e20c1
nathan <nathan.messer@gmail.com>
2012-10-18 23:02:42 +0100

    Formatting tests better

--- test/xml_mapper/core_test.clj
+++ test/xml_mapper/core_test.clj
@@ -11,41 +11,105 @@
   (is (= expected (parse-and-convert xml-text))))
 
 (deftest simple-empty-element  
-  (check {:root nil} 
+  (check {:root nil}
+         
          "<root/>"))
 
 (deftest simple-element
-  (check {:root "text"} 
+  (check {:root "text"}
+         
          "<root>text</root>")) 
 
 (deftest nested-element
-  (check {:root {:elem "text"}} 
-         "<root><elem>text</elem></root>"))
+  (check {:root 
+          {:elem "text"}}
+         
+         "<root>
+             <elem>text</elem>
+          </root>"))
 
 (deftest nested-elements
-  (check {:root {:elem "text" :other "text2"}}
-         "<root><elem>text</elem><other>text2</other></root>"))  
+  (check {:root 
+          {:elem "text" 
+           :other "text2"}}
+         
+         "<root>
+             <elem>text</elem>
+             <other>text2</other>
+          </root>"))  
 
 (deftest nested-same-elements
-  (check {:root {:elem ["text","text2"]}} 
-         "<root><elem>text</elem><elem>text2</elem></root>"))
+  (check {:root 
+          {:elem ["text","text2"]}}
+         
+         "<root>
+             <elem>text</elem>
+             <elem>text2</elem>
+          </root>"))
 
 (deftest mixed-elements
-  (check {:root {:elem "text", :text-content "text2"}}
-          "<root><elem>text</elem>text2</root>"))
+  (check {:root 
+          {:elem "text"
+           :text-content "text2"}}
+         
+          "<root>
+             <elem>text</elem>text2</root>"))
 
 (deftest attributes
-  (check {:root {:attr1 "val"}} 
+  (check {:root 
+          {:attr1 "val"}}
+         
          "<root attr1=\"val\"/>"))
 
 (deftest elements-and-attributes
-  (check {:root {:attr1 "val" :elem "val2"}} 
-         "<root attr1=\"val\"><elem>val2</elem></root>"))
+  (check {:root 
+          {:attr1 "val" 
+           :elem "val2"}} 
+         
+         "<root attr1=\"val\">
+             <elem>val2</elem>
+          </root>"))
 
 (deftest elements-and-attributes-and-text
-  (check {:root {:attr1 "val" :elem "val2" :text-content "text"}} 
+  (check {:root 
+          {:attr1 "val" 
+           :elem "val2" 
+           :text-content "text"}} 
+         
          "<root attr1=\"val\">text<elem>val2</elem></root>"))
 
 (deftest duplicate-attributes-and-elements
-  (check {:root {:key ["attrVal","elemVal"]}} 
-         "<root key=\"attrVal\"><key>elemVal</key></root>"))
+  (check {:root 
+          {:key ["attrVal","elemVal"]}} 
+         
+         "<root key=\"attrVal\">
+             <key>elemVal</key>
+          </root>"))
+
+(deftest deep-nesting
+  (check {:root 
+          {:key 
+              [{:inner ["val1","val2"]},
+               {:inner ["val3","val4"]}]
+           :key2 
+              [{:inner ["val5","val6"]},
+               {:inner ["val7","val8"]}]}}
+         
+         "<root>
+              <key>
+                 <inner>val1</inner>
+                 <inner>val2</inner>
+              </key>
+              <key>
+                 <inner>val3</inner>
+                 <inner>val4</inner>
+             </key>
+             <key2>
+                 <inner>val5</inner>
+                 <inner>val6</inner>
+              </key2>
+              <key2>
+                 <inner>val7</inner>
+                 <inner>val8</inner>
+             </key2>
+          </root>"))
